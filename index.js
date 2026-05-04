762340581fbde99cab1f9e22dfc0ad261b760eb0
Kevin Mercado <141512106+KevinMercado1@users.noreply.github.com>
2026-05-04 15:35:32 -0500

    working ...

--- index.js
+++ index.js
@@ -2,15 +2,19 @@ let btn = document.getElementById('btnNote');
 let input = document.getElementById('noteInput');
 let list = document.getElementById('noteList');
 
-btn.addEventListener('click', () => {
-  let newNotes = document.createElement('li');
-  newNotes.textContent = input.value;
-  list.appendChild(newNotes);
+function addNote() {
+  if (input.value.trim() !== '') {
+    let newNotes = document.createElement('li');
+    newNotes.textContent = input.value;
+    list.appendChild(newNotes);
+    input.value = '';
+  }
+}
 
-  input.value = '';
-  alert('Note added');
+btn.addEventListener('click', addNote);
+
+input.addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') {
+    addNote();
+  }
 });
-
-console.log('Input:', input);
-console.log('Botón:', btn);
-console.log('Lista:', list);
